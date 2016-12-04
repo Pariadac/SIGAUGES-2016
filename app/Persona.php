b4aad2030e6715cedda3e1524df27cd725450b7e
Ely J. Colmenarez M <ElyJColmenarez@gmail.com>
2016-12-04 03:25:58 -0400

    modelo persona terminado (quizas haga falta algun arreglo puesto que tengo una duda en cuanto a una cosita) y sql modificado en la tabla persona (agregado campo tipo_persona)

--- app/Persona.php
+++ app/Persona.php
@@ -10,5 +10,21 @@ class Persona extends Model
     protected $table = "persona";
     protected $primaryKey = "id_persona";
     protected $fillable = ['cedula','nombre','apellido','email','telefono','status','tipo_persona'];
-    
+    protected $guarded = ['id_persona','tipo_persona'];
+
+    public function usuario()
+    {
+        return $this->belongsTo(User::class,'id_usuario','id_persona');
+    }
+
+    public function estudiante()
+    {
+        return $this->belongsTo(Estudiante::class,'id_estudiante','id_persona');
+    }
+
+    public function tutor()
+    {
+        return $this->belongsTo(Tutor::class,'id_tutor','id_persona');
+    }
+
 }
